488ef5c4e4e0d845d1debe5561a55fbb710a0dea
Anderson Contreras <alexandearko@gmail.com>
2019-12-14 15:27:57 -0400

    Agregado axios y creacion del componente que trae datos desde json placeholder

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,28 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          <code>src/App.js</code>
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+class App extends Component {
+	constructor() {
+		super();
+		this.state = {
+			usuarios: []
+		}
+	}
+
+	async componentDidMount() {
+		const respuesta = await axios.get('https://jsonplaceholder.typicode.com/users');
+		this.setState({
+			usuarios: respuesta.data
+		});
+	}
+
+	ponerFilas = () => this.state.usuarios.map((usuario) => (
+		<tr key={ usuario.id }>
+			<td>
+				{ usuario.name }
+			</td>
+			<td>
+				{ usuario.email }
+			</td>
+			<td>
+				{ usuario.website }
+			</td>
+		</tr>
+	));
+
+	render() {
+		return (
+			<div className="margen">
+				<table className="tabla">
+					<thead>
+						<tr>
+							<th>
+								Nombre
+							</th>
+							<th>
+								Correo
+							</th>
+							<th>
+								Enlace
+							</th>
+						</tr>
+					</thead>
+					<tbody>
+						{ this.ponerFilas() }
+					</tbody>
+				</table>
+			</div>
+		)
+	}
+};
 
 export default App;
