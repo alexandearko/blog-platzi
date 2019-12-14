95c16926849c52b21f8400dfc2f6b968b1280ea8
Anderson Contreras <alexandearko@gmail.com>
2019-12-14 19:44:21 -0400

    implementado Redux Thunk

--- src/components/usuarios/index.jsx
+++ src/components/usuarios/index.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import axios from 'axios';
 import { connect } from 'react-redux'
 
 import * as usuariosActions from '../../actions/usuariosActions'
@@ -8,10 +7,6 @@ import * as usuariosActions from '../../actions/usuariosActions'
 class Usuarios extends Component {
 
 	componentDidMount() {
-		// const respuesta = await axios.get('https://jsonplaceholder.typicode.com/users');
-		// this.setState({
-		// 	usuarios: respuesta.data
-		// });
 		this.props.traerTodos();
 	}
 
